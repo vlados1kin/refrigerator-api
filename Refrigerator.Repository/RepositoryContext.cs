3e15602c1db814e8581c4211fff94d7625edef23
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-27 16:31:34 +0300

    create repository context

---
+++ Refrigerator.Repository/RepositoryContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Refrigerator.Domain.Entities;
+
+namespace Refrigerator.Repository;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<Node>? Notes { get; set; }
+    public DbSet<Product>? Products { get; set; }
+    public DbSet<Fridge>? Fridges { get; set; }
+    public DbSet<Model>? Models { get; set; }
+}
