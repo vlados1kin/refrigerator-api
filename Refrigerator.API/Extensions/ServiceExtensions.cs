3e32b2017f49d5c1301987e96439268192a5f619
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-28 19:18:14 +0300

    Create a method to configure repository context

---
+++ Refrigerator.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Refrigerator.Repository;
+
+namespace Refrigerator.API.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureRepositoryContext(this IServiceCollection services, IConfiguration configuration)
+        => services.AddDbContext<RepositoryContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+}
