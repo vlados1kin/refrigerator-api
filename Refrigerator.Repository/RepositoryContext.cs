235900201c57d23f25d7f7cf974ada65e114a006
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-02 13:09:01 +0300

    configure relationships between models

--- Refrigerator.Repository/RepositoryContext.cs
+++ Refrigerator.Repository/RepositoryContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Refrigerator.Domain.Configurations;
 using Refrigerator.Domain.Entities;
 
 namespace Refrigerator.Repository;
@@ -8,20 +7,32 @@ public sealed class RepositoryContext : DbContext
 {
     public RepositoryContext(DbContextOptions options) : base(options)
     {
-        Database.EnsureCreated();
     }
     
-    public DbSet<FridgeProduct>? Nodes { get; set; }
+    public DbSet<FridgeProduct>? FridgeProducts { get; set; }
     public DbSet<Product>? Products { get; set; }
     public DbSet<Fridge>? Fridges { get; set; }
     public DbSet<Model>? Models { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.ApplyConfiguration(new ProductConfiguration());
-        modelBuilder.ApplyConfiguration(new NodeConfiguration());
-        modelBuilder.ApplyConfiguration(new FridgeConfiguration());
-        modelBuilder.ApplyConfiguration(new ModelConfiguration());
+        modelBuilder
+            .Entity<FridgeProduct>()
+            .HasOne(fp => fp.Product)
+            .WithOne(p => p.FridgeProduct)
+            .HasForeignKey<FridgeProduct>(fp => fp.ProductId);
+        
+        modelBuilder
+            .Entity<FridgeProduct>()
+            .HasOne(fp => fp.Fridge)
+            .WithOne(f => f.FridgeProduct)
+            .HasForeignKey<FridgeProduct>(fp => fp.FridgeId);
+        
+        modelBuilder
+            .Entity<Fridge>()
+            .HasOne(f => f.Model)
+            .WithOne(m => m.Fridge)
+            .HasForeignKey<Fridge>(f => f.ModelId);
         
         base.OnModelCreating(modelBuilder);
     }
