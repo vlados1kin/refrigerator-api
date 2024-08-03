2b4da52187146b12236a447647406194ab0a014a
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-04 01:47:11 +0300

    create get fridges method in service

---
+++ Refrigerator.Service/FridgeService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Refrigerator.Contracts.Repository;
+using Refrigerator.Contracts.Service;
+using Refrigerator.Shared.DTO;
+
+namespace Repository.Service;
+
+public class FridgeService : IFridgeService
+{
+    private readonly IRepositoryManager _repositoryManager;
+    private readonly IMapper _mapper;
+    
+    public FridgeService(IRepositoryManager repositoryManager, IMapper mapper)
+    {
+        _repositoryManager = repositoryManager;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<FridgeDto>> GetFridgesAsync(bool trackChanges)
+    {
+        var fridges = await _repositoryManager.Fridge.GetFridgesAsync(trackChanges);
+        var fridgesDto = _mapper.Map<IEnumerable<FridgeDto>>(fridges);
+        return fridgesDto;
+    }
+}
