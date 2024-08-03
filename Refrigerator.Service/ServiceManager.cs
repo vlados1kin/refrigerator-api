40b935d3bf74243e09384b11b07efc6f64bb224d
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-04 01:48:08 +0300

    create service manager

---
+++ Refrigerator.Service/ServiceManager.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Refrigerator.Contracts.Repository;
+using Refrigerator.Contracts.Service;
+using Refrigerator.Repository;
+
+namespace Repository.Service;
+
+public class ServiceManager : IServiceManager
+{
+    private readonly Lazy<IFridgeService> _fridgeService;
+    private readonly Lazy<IProductService> _productService;
+    
+    public ServiceManager(IRepositoryManager repositoryManager, IMapper mapper)
+    {
+        _fridgeService = new Lazy<IFridgeService>(() => new FridgeService(repositoryManager, mapper));
+        _productService = new Lazy<IProductService>(() => new ProductService(repositoryManager, mapper));
+    }
+
+    public IFridgeService FridgeService => _fridgeService.Value;
+    public IProductService ProductService => _productService.Value;
+}
