50d824f76e3ffdd294a3a2f79dc0c173961bc11f
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-04 01:49:55 +0300

    add new fridge controller

---
+++ Refrigerator.Presentation/Controllers/FridgeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Refrigerator.Contracts.Service;
+
+namespace Refrigerator.Presentation.Controllers;
+
+[ApiController]
+[Route("api/fridges")]
+public class FridgeController : ControllerBase
+{
+    private readonly IServiceManager _service;
+
+    public FridgeController(IServiceManager service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetFridges()
+    {
+        var fridges = await _service.FridgeService.GetFridgesAsync(trackChanges: false);
+        return Ok(fridges);
+    }
+}
