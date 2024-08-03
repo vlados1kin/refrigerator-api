7c044efb46711f6c1fd9a9e0e1e8e036aef66e50
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-04 01:44:01 +0300

    create new dtos

---
+++ Refrigerator.Shared/DTO/ModelDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Refrigerator.Shared.DTO;
+
+public record ModelDto
+{
+    public string? Name { get; set; }
+    public int Year { get; init; }
+}
