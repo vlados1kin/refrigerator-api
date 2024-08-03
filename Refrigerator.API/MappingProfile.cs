ed9f1af27967a3991dd896bcec93a2af666a93fa
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-04 01:48:42 +0300

    add new map actions

---
+++ Refrigerator.API/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Refrigerator.Domain.Entities;
+using Refrigerator.Shared.DTO;
+
+namespace Refrigerator.API;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Fridge, FridgeDto>()
+            .ForMember(dest => dest.ModelDto, opt => opt.MapFrom(src => src.Model));
+        CreateMap<Model, ModelDto>();
+    }
+}
