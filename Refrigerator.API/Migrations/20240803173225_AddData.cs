a81f09de4c9a191c72be46a1064934bcbb29a1df
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-04 01:50:41 +0300

    create new migration to add data to database

---
+++ Refrigerator.API/Migrations/20240803173225_AddData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Refrigerator.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Models",
+                columns: new[] { "ModelId", "Name", "Year" },
+                values: new object[,]
+                {
+                    { new Guid("021ca3c1-0deb-4afd-ae94-2159a8479811"), "AB-1234-QQ", 2014 },
+                    { new Guid("80abbca8-664d-4b20-b5de-024705497d4a"), "MC-123-AE", 2015 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Fridges",
+                columns: new[] { "FridgeId", "ModelId", "Name", "OwnerName" },
+                values: new object[,]
+                {
+                    { new Guid("3d490a70-94ce-4d15-9494-5248280c2ce3"), new Guid("021ca3c1-0deb-4afd-ae94-2159a8479811"), "BOSCH", "IITP" },
+                    { new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870"), new Guid("80abbca8-664d-4b20-b5de-024705497d4a"), "Atlant", "IITP" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Fridges",
+                keyColumn: "FridgeId",
+                keyValue: new Guid("3d490a70-94ce-4d15-9494-5248280c2ce3"));
+
+            migrationBuilder.DeleteData(
+                table: "Fridges",
+                keyColumn: "FridgeId",
+                keyValue: new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870"));
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "ModelId",
+                keyValue: new Guid("021ca3c1-0deb-4afd-ae94-2159a8479811"));
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "ModelId",
+                keyValue: new Guid("80abbca8-664d-4b20-b5de-024705497d4a"));
+        }
+    }
+}
